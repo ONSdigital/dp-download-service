f04ca98d0e90a8e17c325a65ce205705d0b17d67
russ-davey <russell.j.davey@gmail.com>
2021-11-24 15:14:55 +0000

    add extra steps

--- features/steps.go
+++ features/steps.go
@@ -8,5 +8,23 @@ func (d *DownloadServiceComponent) iRequestTODownloadTheFile(filename string) er
 
 func (d *DownloadServiceComponent) RegisterSteps(ctx *godog.ScenarioContext) {
         ctx.Step(`^I request to download the file "([^"]*)"$`, d.iRequestTODownloadTheFile)
+        ctx.Step(`^I should receive the private file "([^"]*)"$`, iShouldReceiveThePrivateFile)
+        ctx.Step(`^is not yet published$`, isNotYetPublished)
+        ctx.Step(`^the file "([^"]*)" has been uploaded$`, theFileHasBeenUploaded)
+
 }
+
+func (d *DownloadServiceComponent) iShouldReceiveThePrivateFile(arg1 string) error {
+        return godog.ErrPending
+}
+
+func (d *DownloadServiceComponent) isNotYetPublished() error {
+        return godog.ErrPending
+}
+
+func (d *DownloadServiceComponent) theFileHasBeenUploaded(arg1 string) error {
+        return godog.ErrPending
+}
+
+
 
