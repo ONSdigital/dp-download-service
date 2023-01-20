d6b8a8b4fec5bcd07c884e544aa60a8b1d1dd366
Samin <saminahbab0@gmail.com>
2023-01-20 14:52:47 +0000

    patch: correct xlsx file routes and config
    previously the route was misconfigured and the js config file did not
    include xlsx which has now been added

--- scripts/fix-download-links.js
+++ scripts/fix-download-links.js
@@ -10,68 +10,68 @@
 //
 // note the `/datasets` in the above
 
-instances_coll   = db.getCollection('instances');
-download_formats = { 'xls':1, 'csv':1, 'csvw':1 };      // keys are keys of the `downloads` sub-doc
-debugging        = true;       // when `false`, will change the db!
-lim              = 10;
-count_done       = 0;
+instances_coll = db.getCollection('instances');
+download_formats = { 'xls': 1, 'xlsx': 1, 'csv': 1, 'csvw': 1 };      // keys are keys of the `downloads` sub-doc
+debugging = true;       // when `false`, will change the db!
+lim = 10;
+count_done = 0;
 
-function replace_link(key, oldv, newv){
-        var o             = [ {}, {'$set': {}} ];
-        o[0][key]         = oldv;
-        o[1]['$set'][key] = newv;
-        printjson(o);
-        if (!debugging) {
-            instances_coll.findOneAndUpdate(o[0], o[1])
-        }
-        count_done++;
+function replace_link(key, oldv, newv) {
+  var o = [{}, { '$set': {} }];
+  o[0][key] = oldv;
+  o[1]['$set'][key] = newv;
+  printjson(o);
+  if (!debugging) {
+    instances_coll.findOneAndUpdate(o[0], o[1])
+  }
+  count_done++;
 }
 
 for (dl_fmt in download_formats) {
-    print('processing: ' + dl_fmt);
+  print('processing: ' + dl_fmt);
 
-    q       = {};
-    view    = {};
-    k       = 'downloads.' + dl_fmt + '.href';
-    q[k]    = /onsdigital/;
-    view[k] = true;
-    cursor  = instances_coll.find(q, view).limit(lim);
-    while ( cursor.hasNext() ) {
-        doc    = cursor.next();
-        val    = doc.downloads[dl_fmt].href;
-        newval = val.replace('//download.cmd.onsdigital.co.uk/', '//download.ons.gov.uk/');
-        replace_link(k, val, newval);
-    }
+  q = {};
+  view = {};
+  k = 'downloads.' + dl_fmt + '.href';
+  q[k] = /onsdigital/;
+  view[k] = true;
+  cursor = instances_coll.find(q, view).limit(lim);
+  while (cursor.hasNext()) {
+    doc = cursor.next();
+    val = doc.downloads[dl_fmt].href;
+    newval = val.replace('//download.cmd.onsdigital.co.uk/', '//download.ons.gov.uk/');
+    replace_link(k, val, newval);
+  }
 
-    // -------
+  // -------
 
-    q       = {};
-    view    = {};
-    k       = 'downloads.' + dl_fmt + '.href';
-    q[k]    = /download.beta.ons/;
-    view[k] = true;
-    cursor  = instances_coll.find(q, view).limit(lim);
-    while ( cursor.hasNext() ) {
-        doc    = cursor.next();
-        val    = doc.downloads[dl_fmt].href;
-        newval = val.replace('//download.beta.ons.', '//download.ons.');
-        replace_link(k, val, newval);
-    }
+  q = {};
+  view = {};
+  k = 'downloads.' + dl_fmt + '.href';
+  q[k] = /download.beta.ons/;
+  view[k] = true;
+  cursor = instances_coll.find(q, view).limit(lim);
+  while (cursor.hasNext()) {
+    doc = cursor.next();
+    val = doc.downloads[dl_fmt].href;
+    newval = val.replace('//download.beta.ons.', '//download.ons.');
+    replace_link(k, val, newval);
+  }
 
-    // -------
+  // -------
 
-    q       = {};
-    view    = {};
-    k       = 'downloads.' + dl_fmt + '.public';
-    q[k]    = /static-cmd\.s3/;
-    view[k] = true;
-    cursor  = instances_coll.find(q, view).limit(lim);
-    while ( cursor.hasNext() ) {
-        doc    = cursor.next();
-        val    = doc.downloads[dl_fmt].public;
-        newval = val.replace('//static-cmd.s3', '//ons-dp-production-static.s3');
-        replace_link(k, val, newval);
-    }
+  q = {};
+  view = {};
+  k = 'downloads.' + dl_fmt + '.public';
+  q[k] = /static-cmd\.s3/;
+  view[k] = true;
+  cursor = instances_coll.find(q, view).limit(lim);
+  while (cursor.hasNext()) {
+    doc = cursor.next();
+    val = doc.downloads[dl_fmt].public;
+    newval = val.replace('//static-cmd.s3', '//ons-dp-production-static.s3');
+    replace_link(k, val, newval);
+  }
 
 }
 
