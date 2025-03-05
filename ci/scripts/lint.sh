4959d37e17b8818c6458ef57cb29a6e777c52cb1
aryan-patel <aryanpatel1771@gmail.com>
2025-03-05 15:01:19 +0000

    bumped go version within ci and docker files

--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -3,6 +3,6 @@
 cwd=$(pwd)
 
 pushd $cwd/dp-download-service
-  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
+  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.64.6
   make lint
 popd
